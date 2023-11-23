3fec09e8695b2bbda1ff6522ee4042ef53c93d84
Erica <ericaxue2003@gmail.com>
2023-11-22 21:53:58 -0800

    macbook search page

--- src/main/java/com/example/appleinventoryapp/Main.java
+++ src/main/java/com/example/appleinventoryapp/Main.java
@@ -2,6 +2,7 @@ package com.example.appleinventoryapp;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -11,7 +12,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("login.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400); // v600 v1:400
         stage.setTitle("Apple Login");
         stage.setScene(scene);
         stage.show();
