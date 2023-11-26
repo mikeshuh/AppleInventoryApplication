8b9093c7d0722c801c72e42c448180b3e242ccf8
Erica <ericaxue2003@gmail.com>
2023-11-25 21:52:38 -0800

    login page resize

--- src/main/java/com/example/appleinventoryapp/Main.java
+++ src/main/java/com/example/appleinventoryapp/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("login.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        Scene scene = new Scene(fxmlLoader.load(), 900, 600);
         stage.setTitle("Apple Login");
         stage.setScene(scene);
         stage.show();
